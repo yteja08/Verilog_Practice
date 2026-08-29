bdfd9b4fc3508756ae7e29e9de93f5a090b9b3b6
yteja08 <y.teja0804@gmail.com>
2026-08-29 13:29:29 +0530

    Added RING Successfully

---
+++ 07_Counters/063_Ring_Counter/ring_counter.v
@@ -0,0 +1,14 @@
+module ring_counter(
+input clk,rst, 
+output reg [3:0]q);
+
+always @(posedge clk) begin
+
+    if (rst)
+        q <= 4'b0001;
+    else
+        q <= {q[2:0],q[3]};
+
+end
+
+endmodule
