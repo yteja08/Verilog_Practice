45b0b36d36a17d849f27d3ba7ec9cef70c4ac23b
yteja08 <y.teja0804@gmail.com>
2026-09-06 23:53:32 +0530

    Added Non Overlapping Moore Successfully

---
+++ 08_Finite_State_Machines/070_Sequence_Detector_101/Moore_Sequence_Detector/Non_Overlapping/sequence_detector_101_tb.v
@@ -0,0 +1,32 @@
+module sequence_detector_101_tb();
+
+reg clk, rst, x;
+wire y;
+
+sequence_detector_101 dut (.*);
+
+initial clk = 0;
+always #5 clk = ~clk;
+
+initial begin
+
+$dumpfile ("waveform.vcd");
+$dumpvars (0,sequence_detector_101_tb);
+
+$monitor ("Time = %0t | rst = %b | x = %b | y = %b|", $time , rst, x, y);
+
+rst = 1;x = 0;#10;
+rst = 0;x = 1; #10;
+        x = 1; #10;
+        x = 1; #10;
+	x = 0; #10;
+	x = 1; #30;
+        x = 0; #10;
+        x = 1; #10;
+        x = 0; #30;
+
+$finish;
+
+end
+
+endmodule
