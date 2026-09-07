fb1f14670fee02bcf70b2e26452c4f163b2afade
yteja08 <y.teja0804@gmail.com>
2026-09-07 09:44:42 +0530

    Added Mealy Overlapping 1101 Successfully

---
+++ 08_Finite_State_Machines/071_Sequence_Detector_1101/Mealy_Sequence_Detector/Sequence_detector_1101_tb.v
@@ -0,0 +1,32 @@
+module sequence_detector_1101_tb();
+
+reg clk, rst, x;
+wire y;
+
+sequence_detector_1101 dut (.*);
+
+initial clk = 0;
+always #5 clk = ~clk;
+
+initial begin
+
+$dumpfile ("waveform.vcd");
+$dumpvars (0,sequence_detector_1101_tb);
+
+$monitor ("Time = %0t | rst = %b | x = %b | y = %b|", $time , rst, x, y);
+
+rst = 1;x = 0;#10;
+rst = 0;x = 1; #10;
+        x = 1; #10;
+        x = 1; #10;
+	x = 0; #10;
+	x = 1; #10;
+        x = 1; #10;
+        x = 0; #10;
+        x = 1; #30;
+
+$finish;
+
+end
+
+endmodule
