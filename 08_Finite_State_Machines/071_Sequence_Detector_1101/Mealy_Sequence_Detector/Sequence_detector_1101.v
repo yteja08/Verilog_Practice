7e984353f31880e1b6bbf3c5d3ea2793fd60cb53
yteja08 <y.teja0804@gmail.com>
2026-09-07 09:44:28 +0530

    Added Mealy Overlapping 1101 Successfully

---
+++ 08_Finite_State_Machines/071_Sequence_Detector_1101/Mealy_Sequence_Detector/Sequence_detector_1101.v
@@ -0,0 +1,98 @@
+module sequence_detector_1101 (
+    input clk, rst, x,
+    output reg y
+);					//MEALY OVERLAPPING
+
+parameter S0 = 2'b00,
+          S1 = 2'b01,
+          S2 = 2'b10,
+	  S3 = 2'b11;
+
+reg [1:0] state;
+reg [1:0] next_state;
+
+
+always @(posedge clk or posedge rst) begin
+
+    if (rst)
+        state <= S0;
+    else
+        state <= next_state;
+
+end
+
+
+always @(*) begin
+
+    case (state)
+
+        S0: begin
+            if (x)
+                next_state = S1;
+            else
+                next_state = S0;
+        end
+
+
+        S1: begin
+            if (x)
+                next_state = S2;
+            else
+                next_state = S0;
+        end
+
+
+        S2: begin
+            if (x)
+                next_state = S2;
+            else
+                next_state = S3;
+        end
+
+	 S3: begin
+            if (x)
+                next_state = S1;
+            else
+                next_state = S0;
+        end
+
+
+        default:
+            next_state = S0;
+
+    endcase
+
+end
+
+
+always @(*) begin
+
+    case (state)
+
+        S0: begin
+            y = 1'b0;
+        end
+
+        S1: begin
+            y = 1'b0;
+        end
+	
+	S2: begin
+	   y = 1'b0;
+	end
+
+        S3: begin
+            if (x)
+                y = 1'b1;
+            else
+                y = 1'b0;
+        end
+
+        default:
+            y = 1'b0;
+
+    endcase
+
+end
+
+endmodule
