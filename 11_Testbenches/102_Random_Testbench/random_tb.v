bedfadd81dfc14e5f97460988390656b9b5ac06e
yteja08 <y.teja0804@gmail.com>
2026-09-15 10:23:33 +0530

    Added Random Generation TB Successfully

---
+++ 11_Testbenches/102_Random_Testbench/random_tb.v
@@ -0,0 +1,41 @@
+module adder_tb();
+
+reg [3:0] a;
+reg [3:0] b;
+
+wire [4:0] sum;
+
+reg  [4:0] expected;
+
+integer i;
+
+adder dut (.*);
+
+initial begin
+
+    $dumpfile("waveform.vcd");
+    $dumpvars(0, adder_tb);
+
+    for (i = 0; i < 20; i = i + 1) begin
+
+        a = $random;
+        b = $random;
+
+        expected = a + b;
+
+        if (sum == expected)
+            $display("PASS: a=%d b=%d expected=%d actual=%d",
+                     a, b, expected, sum);
+        else
+            $display("FAIL: a=%d b=%d expected=%d actual=%d",
+                     a, b, expected, sum);
+
+	#10;
+
+    end
+
+    $finish;
+
+end
+
+endmodule
